b3154c0f78bbe3c71fac59c6af687ac41787caaa
acp <prepodam@mail.ru>
2016-09-06 16:57:34 +0300

    advertise-wan to consul.
    Incorrect address.

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -4,4 +4,6 @@ WEAVE_IP=$( hostname -i | awk '{print $1}' )
 
 JOIN_WAN=$( drill consul.weave.local | fgrep IN | fgrep -v ';' | awk '{print $5}' | sed 's/^/-join-wan /' | tr '\n' ' ' | sed 's/,$/\n/' )
 
-docker-entrypoint.sh agent -advertise $WEAVE_IP $JOIN_WAN $@
+echo "Start: docker-entrypoint.sh agent -advertise $WEAVE_IP -advertise-wan $WEAVE_IP $JOIN_WAN $@"
+
+docker-entrypoint.sh agent -advertise $WEAVE_IP -advertise-wan $WEAVE_IP $JOIN_WAN $@
