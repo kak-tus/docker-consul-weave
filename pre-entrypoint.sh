a444fd9b8c342eaa25d669e1e03efb92e46b7f33
acp <prepodam@mail.ru>
2016-09-06 16:24:25 +0300

    May be no quotes needed?

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -4,4 +4,4 @@ WEAVE_IP=$( hostname -i | awk '{print $1}' )
 
 JOIN_WAN=$( drill consul.weave.local | fgrep IN | fgrep -v ';' | awk '{print $5}' | sed 's/^/-join-wan /' | tr '\n' ' ' | sed 's/,$/\n/' )
 
-docker-entrypoint.sh consul agent -advertise $WEAVE_IP $JOIN_WAN "$@"
+docker-entrypoint.sh consul agent -advertise $WEAVE_IP $JOIN_WAN $@
