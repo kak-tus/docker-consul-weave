cddca00b2c20b36e85e8f6719f18428db3bee563
acp <prepodam@mail.ru>
2016-09-08 03:33:14 +0300

    Wan advertise is not needed.
    Sane as echo for debug.

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -4,6 +4,4 @@ WEAVE_IP=$( hostname -i | awk '{print $1}' )
 
 JOIN_WAN=$( drill consul.weave.local | fgrep IN | fgrep -v ';' | awk '{print $5}' | sed 's/^/-join-wan /' | tr '\n' ' ' | sed 's/,$/\n/' )
 
-echo "Start: docker-entrypoint.sh agent -advertise $WEAVE_IP -advertise-wan $WEAVE_IP $JOIN_WAN $@"
-
-docker-entrypoint.sh agent -advertise $WEAVE_IP -advertise-wan $WEAVE_IP $JOIN_WAN $@
+docker-entrypoint.sh agent -advertise $WEAVE_IP $JOIN_WAN $@
