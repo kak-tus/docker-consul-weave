73d67b69494f3156bddecf948ea260f12a3d06ae
acp <prepodam@mail.ru>
2016-10-07 22:21:03 +0300

    Support of multi-consult in one DC.
    Before this version if we start multi consul servers in one DC, they connect to each other with join-wan, not join. It's not correct feature.
    Now we support correct join.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM consul:v0.6.4
 
 COPY pre-entrypoint.sh /bin/pre-entrypoint.sh
 
+ENV DC=
+ENV DCS=
+
 RUN apk add --no-cache drill
 
 ENTRYPOINT ["/bin/pre-entrypoint.sh"]
--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -1,7 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 WEAVE_IP=$( hostname -i | awk '{print $1}' )
 
-JOIN_WAN=$( drill consul.weave.local | fgrep IN | fgrep -v ';' | awk '{print $5}' | sed 's/^/-join-wan /' | tr '\n' ' ' | sed 's/,$/\n/' )
+DC_LIST=$( echo $DCS | tr "," "\n" )
+JOIN=""
+JOIN_WAN=""
 
-docker-entrypoint.sh agent -advertise $WEAVE_IP $JOIN_WAN $@
+for i in $DC_LIST
+do
+  if [ $i == $DC ]; then
+    JOIN=$( drill $i.consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $WEAVE_IP | awk '{print $5}' | tr "\n" "," | sed 's/,$/\n/' )
+  else
+    IPS=$( drill $i.consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $WEAVE_IP | awk '{print $5}' | tr "\n" "," | sed 's/,$/\n/' )
+    JOIN_WAN=$( echo "$IPS,$JOIN_WAN" | sed 's/,$/\n/')
+  fi
+done
+
+if [ -n "$JOIN" ]; then
+  JOIN=$( echo $JOIN | tr "," "\n" | sed 's/^/-retry-join /' | tr "\n" " " | sed 's/,$/\n/' )
+fi
+
+if [ -n "$JOIN_WAN" ]; then
+  JOIN_WAN=$( echo $JOIN_WAN | tr "," "\n" | sed 's/^/-retry-join-wan /' | tr "\n" " " | sed 's/,$/\n/' )
+fi
+
+echo "docker-entrypoint.sh agent -advertise $WEAVE_IP -retry-max 5 -retry-max-wan 5 $JOIN $JOIN_WAN $@"
