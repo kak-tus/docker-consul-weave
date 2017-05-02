a7594dff8e7008184a43dc7580dd1c781fc4d96e
Andrey Kuzmin <prepodam@mail.ru>
2017-05-03 00:40:11 +0300

    Direct IP list support.
    Weave is good, but slow network. So, try to use consul without weave. Consul connectivity may be done with ansible as example.

--- pre-entrypoint.sh
+++ pre-entrypoint.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env sh
 
-WEAVE_IP=$( hostname -i | awk '{print $1}' )
+if [ -z "$CONSUL_WEAVE_IP" ]; then
+  CONSUL_WEAVE_IP=$( hostname -i | awk '{print $1}' )
+fi
 
-DC_LIST=$( echo $DCS | tr "," "\n" )
-JOIN=""
-JOIN_WAN=""
+if [ -z "${CONSUL_WEAVE_JOIN}${CONSUL_WEAVE_JOIN_WAN}" ]; then
+  DC_LIST=$( echo $DCS | tr "," "\n" )
 
-for i in $DC_LIST
-do
-  if [ $i == $DC ]; then
-    JOIN=$( drill $i.consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $WEAVE_IP | awk '{print $5}' | grep -E -o '^[0-9\.]+$' | tr "\n" "," | sed 's/,$//' )
-  else
-    IPS=$( drill $i.consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $WEAVE_IP | awk '{print $5}' | grep -E -o '^[0-9\.]+$' | tr "\n" "," | sed 's/,$//' )
-    if [ -n "$IPS" ]; then
-      JOIN_WAN=$( echo "$IPS,$JOIN_WAN" | sed 's/,$//')
+  for i in $DC_LIST
+  do
+    if [ $i == $DC ]; then
+      CONSUL_WEAVE_JOIN=$( drill $i.consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $CONSUL_WEAVE_IP | awk '{print $5}' | grep -E -o '^[0-9\.]+$' | tr "\n" "," | sed 's/,$//' )
+    else
+      IPS=$( drill $i.consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $CONSUL_WEAVE_IP | awk '{print $5}' | grep -E -o '^[0-9\.]+$' | tr "\n" "," | sed 's/,$//' )
+      if [ -n "$IPS" ]; then
+        CONSUL_WEAVE_JOIN_WAN=$( echo "$IPS,$CONSUL_WEAVE_JOIN_WAN" | sed 's/,$//')
+      fi
     fi
-  fi
-done
-
-JOIN_WAN_COMPATIBLE=$( drill consul.weave.local | fgrep IN | fgrep -v ';' | fgrep -v $WEAVE_IP | awk '{print $5}' | grep -E -o '^[0-9\.]+$' | tr "\n" "," | sed 's/,$//' )
+  done
+fi
 
 bootstrap=""
 
-if [ -n "$JOIN" ]; then
-  JOIN=$( echo $JOIN | tr "," "\n" | sed 's/^/-retry-join /' | tr "\n" " " | sed 's/,$//' )
+if [ -n "$CONSUL_WEAVE_JOIN" ]; then
+  CONSUL_WEAVE_JOIN=$( echo $CONSUL_WEAVE_JOIN | tr "," "\n" | sed 's/^/-retry-join /' | tr "\n" " " | sed 's/,$//' )
 else
   bootstrap="-bootstrap"
 fi
 
-if [ -n "$JOIN_WAN" ]; then
-  JOIN_WAN=$( echo $JOIN_WAN | tr "," "\n" | sed 's/^/-retry-join-wan /' | tr "\n" " " | sed 's/,$//' )
+if [ -n "$CONSUL_WEAVE_JOIN_WAN" ]; then
+  CONSUL_WEAVE_JOIN_WAN=$( echo $CONSUL_WEAVE_JOIN_WAN | tr "," "\n" | sed 's/^/-retry-join-wan /' | tr "\n" " " | sed 's/,$//' )
 fi
 
-if [ -n "$JOIN_WAN_COMPATIBLE" ]; then
-  JOIN_WAN_COMPATIBLE=$( echo $JOIN_WAN_COMPATIBLE | tr "," "\n" | sed 's/^/-retry-join-wan /' | tr "\n" " " | sed 's/,$//' )
-fi
-
-docker-entrypoint.sh agent $bootstrap -advertise $WEAVE_IP -retry-max 5 -retry-max-wan 5 $JOIN $JOIN_WAN $JOIN_WAN_COMPATIBLE $@ &
+docker-entrypoint.sh agent $bootstrap -advertise $CONSUL_WEAVE_IP -retry-max 5 -retry-max-wan 5 $CONSUL_WEAVE_JOIN $CONSUL_WEAVE_JOIN_WAN $@ &
 child=$!
 
 trap "kill $child" SIGTERM SIGINT
